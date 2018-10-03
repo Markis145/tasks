e595c9d6dea78cd605148a53d8c7a0597e939395
Markis145 <marc_mstr@hotmail.com>
2018-10-03 18:02:25 +0200

    inici vue

---
+++ vue-tasks/src/components/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul>
+        <li>tasca1</li>
+        <li>tasca1</li>
+        <li>tasca1</li>
+        <li>tasca1</li>
+        <li>tasca1</li>
+    </ul>
+
+</template>
+
+<script>
+
+
+</script>
+
+<styles>
+
+
+</styles>
