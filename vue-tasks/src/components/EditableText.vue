f9dd723f594c1ac11a79de8ea169e017417a4bc2
Markis145 <marc_mstr@hotmail.com>
2018-10-05 18:31:52 +0200

    sync icones edit

--- vue-tasks/src/components/EditableText.vue
+++ vue-tasks/src/components/EditableText.vue
@@ -1,42 +1,43 @@
 <template>
-    <span>
-        <span v-if="!editing" @dblclick="editing=true">
-            {{ currentText }}
-        </span>
-        <span v-if="editing" @keyup.esc="editing=false"
-                @keyup.enter="edit">
-            <input type="text" :value="text">
-            <!--<input type="text" :value="text" @input="text= $event.target.value">-->
-            <input type="text" v-model="currentText">
-        </span>
+  <span>
+      <span v-if="!editing" @dblclick="editing=true">
+          {{text}} &nbsp;
+          <svg @click="editing=true" class="h-3 w-4.5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.3 3.7l4 4L4 20H0v-4L12.3 3.7zm1.4-1.4L16 0l4 4-2.3 2.3-4-4z"/></svg> <!-- Edit -->
+      </span>
 
-    </span>
+      <span v-if="editing" @keyup.esc="editing=false" @keyup.enter="edit">
+          <svg @click="edit" class="h-3 w-3.5  fill-current text-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg> <!-- Check -->
+          <svg @click="edit" class="h-3 w-3.5  fill-current text-red" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/></svg>&nbsp; <!--Cancel -->
+
+          <input type="text" v-model="currentText">
+          <!--//SINTAX SUGAR -->
+          <!--<input type="text" :value="text" @input="text = $event.target.value">-->
+      </span>
+  </span>
 </template>
-
 <script>
-
     export default {
         name: 'EditableText',
-        data(){
+        data() {
             return {
-                editing:false,
+                editing: false,
                 currentText: this.text
             }
         },
+        // props :['text'],
         props: {
             'text': {
                 type: String,
-                required: true
+                required : true
             }
-        }   ,
+        },
         // props: ['text'],
         methods: {
-            edit(){
+            edit() {
                 this.editing = false
                 //informat al pare
                 this.$emit('edited',this.currentText)
             }
         }
     }
-
 </script>
--- vue-tasks/src/components/Tasks.vue
+++ vue-tasks/src/components/Tasks.vue
@@ -234,64 +234,61 @@
 <!--</style>-->
 
 <template>
-    <div class="container flex justify-center">
+    <div class="flex justify-center">
         <div class="flex flex-col">
-            <h1 class="text-center text-red-light pb-3 pt-8">Tasques ({{total}})</h1>
-            <div class="flex-row">
-                <input type="text" v-model="newTask" @keyup.enter="add" placeholder="Nova Tasca"
+            <h1 class="text-center text-red-light">Tasques({{total}}) </h1>
+            <div class="flex-row"  >
+
+                <input type="text" placeholder="Nova Tasca"
+                       v-model="newTask" @keyup.enter="add"
                        class="m-3 mt-5 p-2 pl-5 shadow border rounded focus:outline-none focus:shadow-outline text-grey-darker">
-                <!--<button @click="add">Afegir</button>-->
-                <svg @click="add" class="h-4 w-4 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                    <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z"/>
-                </svg>
+                <button @click="add" class="text-center text-red"  >Afegir</button>
             </div>
-            <!--<input :value="newTask" @input="newTask = $event.target.value">-->
-            <ul class="list-reset">
-                <!--<li v-for="task in tasks" v-if="task.completed"><strike>{{task.name}}</strike></li>-->
-                <!--<li v-else>{{task.name}}</li>-->
-                <li v-for="task in filteredTasks" :key="task.id" class="text-grey-darker m-2 pl-5">
-                    <span :class="{strike:task.completed}">
-                        <editable-text :text="task.name" @edited="editName(task, $event)">
-                            <!--{{task.name}}-->
-                        </editable-text>
-                    </span>
-                    &nbsp;
-                    <span @click="remove(task)" class="cursor-pointer">&#215;</span>
+            <!-- -->
+            <ul>
+                <li v-for="task in filteredTasks" :key="task.id">
+                <span :class="{ strike: task.completed }">
+                    <editable-text
+                            :text="task.name" @edited="editName(task, $event)"
+                    ></editable-text>
+                </span>
+                    <span @click="remove(task)">&#215;</span>
                 </li>
             </ul>
-            <h3>Filtros</h3>
-            Active filter: {{filter}}
-            <ul class="list-reset">
-                <li>
-                    <button @click="setFilter('all')">Totes</button>
-                </li>
-                <li>
-                    <button @click="setFilter('completed')">Completades</button>
-                </li>
-                <li>
-                    <button @click="setFilter('active')">Pendents</button>
-                </li>
+            <!-- -->
+            <br>
+            <h3>Filtros:</h3>
+            <br>
+            Filtre emprat -> {{ filter }}
+
+            <ul>
+                <br>
+                <li><button @click="setFilter('all')">Totes</button></li>
+                <li><button @click="setFilter('completed')">Completades</button></li>
+                <li><button @click="setFilter('active')">Pendents</button></li>
             </ul>
         </div>
     </div>
 </template>
-
 <script>
-    import EditableText from './EditableText.vue'
+    import EditableText from './EditableText'
     var filters = {
-        all: function (tasks) {
+        all: function(tasks) {
             return tasks
         },
-        completed: function (tasks) {
+        completed: function(tasks) {
             return tasks.filter(function (task) {
                 return task.completed
+                // NO CAL
+                // if (task.completed) return true
+                // else return false
             })
         },
-        active: function (tasks) {
+        active: function(tasks) {
             return tasks.filter(function (task) {
                 return !task.completed
             })
-        }
+        },
     }
     export default {
         components: {
@@ -299,12 +296,24 @@
         },
         data() {
             return {
-                filter: 'all',
+                filter: 'all', // All Completed Active
                 newTask: '',
                 tasks: [
-                    {id: 1, name: 'Comprar pa', completed: false},
-                    {id: 2, name: 'Comprar llet', completed: false},
-                    {id: 3, name: 'Comprar patates', completed: true}
+                    {
+                        id: 1,
+                        name: 'Comprar pa',
+                        completed: false
+                    },
+                    {
+                        id: 2,
+                        name: 'Comprar llet',
+                        completed: false
+                    },
+                    {
+                        id: 3,
+                        name: 'Estudiar PHP',
+                        completed: true
+                    }
                 ]
             }
         },
@@ -313,28 +322,29 @@
                 return this.tasks.length
             },
             filteredTasks() {
+                // Segons el filtre actiu
+                // Alternativa switch/case -> array associatiu
                 return filters[this.filter](this.tasks)
             }
         },
         methods: {
-            editName(task, text) {
-                // console.log('TASK: ', task.name);
-                // console.log('TEXT: ', text);
-                // console.log("TODO editName");
-                task.name = text
+            editName(task,text){
+                task.name=text
             },
             setFilter(newFilter) {
                 this.filter = newFilter
             },
             add() {
-                if (this.newTask === "") return
-                this.tasks.splice(0, 0, {name: this.newTask, completed: false})
-                this.newTask = ""
+                this.tasks.splice(0,0,{ name: this.newTask, completed: false } )
+                this.newTask=''
             },
             remove(task) {
                 window.console.log(task)
-                this.tasks.splice(this.tasks.indexOf(task), 1)
+                this.tasks.splice(this.tasks.indexOf(task),1)
             }
+            // edit(task){
+            //
+            // }
         }
     }
 </script>
