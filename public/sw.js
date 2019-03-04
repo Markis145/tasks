98b58af33444be3fa5c129d0d7ebdffb3a942dc8
Markis145 <marc_mstr@hotmail.com>
2019-03-04 12:04:34 +0100

    Sync

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/service-worker/precache-manifest.a747ad69846a1dd7b9c4baac63914483.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/service-worker/precache-manifest.b1443e7b1234985bd899227611022b78.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting()
 workbox.clientsClaim()
