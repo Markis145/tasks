c45f64f9d32b57b72701ddd0b99d2d264acc0aec
Markis145 <marc_mstr@hotmail.com>
2019-05-29 18:58:33 +0200

    sync

--- app/Listeners/LogTaskModify.php
+++ app/Listeners/LogTaskModify.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Auth;
 
-class LogTaskModify
+class LogTaskModify implements ShouldQueue
 {
     /**
      * Create the event listener.
